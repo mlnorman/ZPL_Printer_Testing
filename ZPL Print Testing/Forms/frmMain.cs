f3e0ee199b6691991b952983d6d29067e5a68809
Michael Norman <MichaelNorman@packagingcorp.com>
2022-03-01 09:06:47 -0500

    removed missing dependecy from usings

--- ZPL Print Testing/Forms/frmMain.cs
+++ ZPL Print Testing/Forms/frmMain.cs
@@ -11,7 +11,6 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using BinaryKits.Zpl.Viewer;
 using ZPL_Print_Testing.Forms;
 using ZPL_Print_Testing.Models;
 using ZPL_Print_Testing.Servers;
